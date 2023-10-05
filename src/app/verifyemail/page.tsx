15358576820f26ddb0a505009f5f8bad8bbb1b88
Deepak Gupta <deepak.gupta@sts.local>
2023-10-05 11:05:23 +0530

    uPPERCASE

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
-export default function verifyEmailPage() {
+export default function VerifyEmailPage() {
   const [token, setToken] = React.useState("");
   const [verified, setVerified] = React.useState(false);
   const [error, setError] = React.useState(false);
